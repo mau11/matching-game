c3a9fd925b2d4142fbdfaa1b70666a876e3a2367
Maureen E. Mirville <maureen.mirville@gmail.com>
2020-04-19 12:56:12 -0400

    Add jsx component and set up webpack

---
+++ src/components/Layout.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+export default class Layout extends Component {
+
+  render() {
+    return (
+      <h1>The Matching Game</h1>
+    )
+  }
+}
